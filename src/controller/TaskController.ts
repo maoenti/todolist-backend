b67969ce3c923313a28e154a57aaf1dcd92b8504
melvynDoCheck <melvyn@docheck.id>
2022-11-07 13:41:58 +0700

    implement prettier

--- src/controller/TaskController.ts
+++ src/controller/TaskController.ts
@@ -1,31 +1,32 @@
-import { AppDataSource } from "../data-source"
-import { NextFunction, Request, Response } from "express"
-import { Task } from "../entity/Task"
+import { AppDataSource } from "../data-source";
+import { NextFunction, Request, Response } from "express";
+import { Task } from "../entity/Task";
 
 export class TaskController {
+  private userRepository = AppDataSource.getRepository(Task);
 
-    private userRepository = AppDataSource.getRepository(Task);
+  async all(request: Request, response: Response, next: NextFunction) {
+    return this.userRepository.find();
+  }
 
-    async all(request: Request, response: Response, next: NextFunction) {
-        return this.userRepository.find()
-    }
+  async one(request: Request, response: Response, next: NextFunction) {
+    return this.userRepository.findOneByOrFail({ id: request.params.id });
+  }
 
-    async one(request: Request, response: Response, next: NextFunction) {
-        return this.userRepository.findOneByOrFail({id: request.params.id})
-    }
+  async save(request: Request, response: Response, next: NextFunction) {
+    return this.userRepository.save(request.body);
+  }
 
-    async save(request: Request, response: Response, next: NextFunction) {
-        return this.userRepository.save(request.body)
-    }
+  async update(request: Request, response: Response, next: NextFunction) {
+    await this.userRepository.findOneByOrFail({ id: request.params.id });
+    await this.userRepository.update({ id: request.params.id }, request.body);
+    return this.userRepository.findOneByOrFail({ id: request.params.id });
+  }
 
-    async update(request: Request, response: Response, next: NextFunction) {
-        await this.userRepository.findOneByOrFail({ id: request.params.id })
-        await this.userRepository.update({ id: request.params.id }, request.body)
-        return this.userRepository.findOneByOrFail({id: request.params.id})
-    }
-
-    async remove(request: Request, response: Response, next: NextFunction) {
-        const userToRemove = await this.userRepository.findOneByOrFail({ id: request.params.id })
-        return this.userRepository.remove(userToRemove)
-    }
+  async remove(request: Request, response: Response, next: NextFunction) {
+    const userToRemove = await this.userRepository.findOneByOrFail({
+      id: request.params.id,
+    });
+    return this.userRepository.remove(userToRemove);
+  }
 }
--- src/controller/UsersController.ts
+++ src/controller/UsersController.ts
@@ -1,31 +1,31 @@
-import { AppDataSource } from "../data-source"
-import { NextFunction, Request, Response } from "express"
-import { Users } from "../entity/Users"
+import { AppDataSource } from "../data-source";
+import { NextFunction, Request, Response } from "express";
+import { Users } from "../entity/Users";
 
 export class UsersController {
+  private userRepository = AppDataSource.getRepository(Users);
 
-    private userRepository = AppDataSource.getRepository(Users);
+  async all(request: Request, response: Response, next: NextFunction) {
+    return this.userRepository.find();
+  }
 
-    async all(request: Request, response: Response, next: NextFunction) {
-        return this.userRepository.find()
-    }
+  async one(request: Request, response: Response, next: NextFunction) {
+    return this.userRepository.findOneByOrFail({ id: request.params.id });
+  }
 
-    async one(request: Request, response: Response, next: NextFunction) {
-        return this.userRepository.findOneByOrFail({id: request.params.id})
-    }
+  async save(request: Request, response: Response, next: NextFunction) {
+    return this.userRepository.save(request.body);
+  }
 
-    async save(request: Request, response: Response, next: NextFunction) {
-        return this.userRepository.save(request.body)
-    }
-
-    async update(request: Request, response: Response, next: NextFunction) {
-        await this.userRepository.findOneByOrFail({ id: request.params.id })
-        await this.userRepository.update({ id: request.params.id }, request.body)
-        return this.userRepository.findOneByOrFail({id: request.params.id})
-    }
-
-    async remove(request: Request, response: Response, next: NextFunction) {
-        const userToRemove = await this.userRepository.findOneByOrFail({ id: request.params.id })
-        return this.userRepository.remove(userToRemove)
-    }
+  async update(request: Request, response: Response, next: NextFunction) {
+    await this.userRepository.findOneByOrFail({ id: request.params.id });
+    await this.userRepository.update({ id: request.params.id }, request.body);
+    return this.userRepository.findOneByOrFail({ id: request.params.id });
+  }
+  async remove(request: Request, response: Response, next: NextFunction) {
+    const userToRemove = await this.userRepository.findOneByOrFail({
+      id: request.params.id,
+    });
+    return this.userRepository.remove(userToRemove);
+  }
 }
